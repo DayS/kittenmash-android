36bece08602c020cde5c85aa4887ca7448d23856
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-11-19 22:26:18 +0100

    replace with XML

--- src/fr/lyonjug/kittenmash/BestKittenActivity.java
+++ src/fr/lyonjug/kittenmash/BestKittenActivity.java
@@ -2,9 +2,6 @@ package fr.lyonjug.kittenmash;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class BestKittenActivity extends Activity {
 
@@ -12,17 +9,6 @@ public class BestKittenActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Button button = new Button(this);
-		button.setText("Fight!");
-
-		ImageView imageView = new ImageView(this);
-		imageView.setImageResource(R.drawable.kitten_0);
-
-		LinearLayout linearLayout = new LinearLayout(this);
-		linearLayout.setOrientation(LinearLayout.VERTICAL);
-		linearLayout.addView(button);
-		linearLayout.addView(imageView);
-
-		setContentView(linearLayout);
+		setContentView(R.layout.activity_best_kitten);
 	}
 }
