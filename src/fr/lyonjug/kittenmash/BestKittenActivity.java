71337eae28e9295c7acd81b2a0d6599fe0e5822d
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-11-19 22:44:50 +0100

    showing random kittens

--- src/fr/lyonjug/kittenmash/BestKittenActivity.java
+++ src/fr/lyonjug/kittenmash/BestKittenActivity.java
@@ -1,5 +1,7 @@
 package fr.lyonjug.kittenmash;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +9,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 public class BestKittenActivity extends Activity {
+
+	private int[] kittenResIds;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +26,29 @@ public class BestKittenActivity extends Activity {
 			}
 		});
 
+		kittenResIds = new int[] { R.drawable.kitten_0, //
+				R.drawable.kitten_1, //
+				R.drawable.kitten_2, //
+				R.drawable.kitten_3, //
+				R.drawable.kitten_4,//
+				R.drawable.kitten_5, //
+				R.drawable.kitten_6, //
+				R.drawable.kitten_7, //
+				R.drawable.kitten_8, //
+				R.drawable.kitten_9 //
+		};
+
 	}
 
 	protected void fight() {
 		Intent intent = new Intent(this, KittenFightActivity.class);
 
-		intent.putExtra(KittenFightActivity.KITTEN1_EXTRA, R.drawable.kitten_0);
-		intent.putExtra(KittenFightActivity.KITTEN2_EXTRA, R.drawable.kitten_1);
+		Random random = new Random();
+		int kitten1 = kittenResIds[random.nextInt(kittenResIds.length)];
+		int kitten2 = kittenResIds[random.nextInt(kittenResIds.length)];
+
+		intent.putExtra(KittenFightActivity.KITTEN1_EXTRA, kitten1);
+		intent.putExtra(KittenFightActivity.KITTEN2_EXTRA, kitten2);
 
 		startActivity(intent);
 	}
