6cbccec5a20650c8a406fc9dc52bdfcc642d0572
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-11-19 22:20:51 +0100

    button and image view at size

--- src/fr/lyonjug/kittenmash/BestKittenActivity.java
+++ src/fr/lyonjug/kittenmash/BestKittenActivity.java
@@ -2,7 +2,9 @@ package fr.lyonjug.kittenmash;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class BestKittenActivity extends Activity {
 
@@ -10,10 +12,17 @@ public class BestKittenActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		Button button = new Button(this);
+		button.setText("Fight!");
+
 		ImageView imageView = new ImageView(this);
 		imageView.setImageResource(R.drawable.kitten_0);
 
-		setContentView(imageView);
-	}
+		LinearLayout linearLayout = new LinearLayout(this);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		linearLayout.addView(button);
+		linearLayout.addView(imageView);
 
+		setContentView(linearLayout);
+	}
 }
